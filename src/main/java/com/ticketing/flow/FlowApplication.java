8c56f30ec61e6abbcd7a3fcd309f2f3b18bbb9ba
yujinlee <now8567@hanmail.net>
2025-01-30 17:02:53 +0900

    대기열 스케줄러 개발

--- src/main/java/com/ticketing/flow/FlowApplication.java
+++ src/main/java/com/ticketing/flow/FlowApplication.java
@@ -2,8 +2,10 @@ package com.ticketing.flow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FlowApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(FlowApplication.class, args);
---
+++ src/main/java/com/ticketing/flow/service/UserQueueScheduleService.java
@@ -0,0 +1,39 @@
+package com.ticketing.flow.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserQueueScheduleService {
+    private final UserQueueService userQueueService;
+    private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
+    private static final String USER_QUEUE_WAIT_SCAN_KEY = "users:queue:*:wait";
+
+    @Value("${scheduler.enabled}")
+    private Boolean scheduling = false;
+
+    // 서버 시작 후 5초 대기 , 3초 주기로 실행
+    @Scheduled(initialDelay = 5000, fixedDelay = 3000)
+    public void scheduleAllowUser() {
+        if(!scheduling){
+            return;
+        }
+        long maxAllowedUserCount = 3;
+        // 사용자 허용
+        reactiveRedisTemplate.scan(ScanOptions.scanOptions()
+                        .match(USER_QUEUE_WAIT_SCAN_KEY)
+                        .count(100)
+                        .build())
+                .map(key -> key.toString().split(":")[2])
+                .flatMap(queue -> userQueueService.allowUser(queue.toString(), maxAllowedUserCount))
+                .doOnNext(allowed -> log.info("Tried %d and allowed %d members".formatted(maxAllowedUserCount, allowed)))
+                .subscribe();
+    }
+}
